5ed572e0275fc4012e3748a7148a342b0ad8bcf3
0xHuge <117529459+huge8888@users.noreply.github.com>
2024-12-31 15:11:44 +0700

    Add files via upload

---
+++ account.js
@@ -0,0 +1,18 @@
+const accountLists = [
+    {
+      "username": "xxxx",
+      "password": "xxxx"
+    },
+    {
+      "username": "xxxx",
+      "password": "xxxx"
+    },
+    {
+      "username": "xxxx",
+      "password": "xxxx"
+    } // for more users just copy paste the form as many as your accounts
+];
+
+module.exports = {
+    accountLists
+};
